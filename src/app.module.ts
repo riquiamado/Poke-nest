4cc796f9cd328e10c147a8acf5165259bba96e1f
riquiamado <carlosamado221471@gmail.com>
2024-10-02 01:33:11 -0300

    resource pokemon instalado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PokemonModule } from './pokemon/pokemon.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PokemonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
