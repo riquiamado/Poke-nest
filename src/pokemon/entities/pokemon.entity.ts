6842a3291b2fa19e6b03b9b512d344a589aa27ad
riquiamado <carlosamado221471@gmail.com>
2024-10-02 02:32:20 -0300

    entity y conexion a base de datos

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,11 @@
-export class Pokemon {}
+import { Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export class Pokemon extends Document {
+  @Prop({ unique: true, index: true })
+  no: number;
+  @Prop({ unique: true, index: true })
+  name: string;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
